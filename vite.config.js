212fc22cbd0e353fd35e1dfb21498d228b007d77
xyz17-dom <mimilmilea07@gmail.com>
2026-07-09 15:58:36 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/correspondence-system/', // <--- Bagian ini wajib ada agar aset CSS/JS Anda tidak hilang!
+  base: '/', // <--- Bagian ini wajib ada agar aset CSS/JS Anda tidak hilang!
 })
